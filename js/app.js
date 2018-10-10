91218a736337e46560149c4a13d854ea667f99f3
sohaghareb <soha.ghareb@student.guc.edu.eg>
2018-10-10 19:56:05 +0200

    fixed correct animation in css class

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ let moves=0;
 let children="";
 let stars=2;
 let htmlList=document.querySelector('.deck');
+
 function init(){
 
     let array=['fa-diamond','fa-diamond','fa-paper-plane-o','fa-paper-plane-o',
@@ -36,10 +37,7 @@ function init(){
     li_list.forEach(function(i){
         i.className="fa fa-star";
     });
-        
-
 }
-
 /**Start of Events **/
 htmlList.onclick= function(event){
     let target=event.target;
@@ -63,10 +61,8 @@ htmlList.onclick= function(event){
             }
             firstClick='';
             incrementMoves();
-        }
-        
-    }
-    
+        }    
+    }   
 }
 
 document.querySelector('.restart').onclick=function(){
@@ -91,7 +87,6 @@ document.querySelector('.mybutton').onclick=function(){
         }, 501);
     },501);
 }
-
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -103,7 +98,6 @@ document.querySelector('.mybutton').onclick=function(){
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -167,15 +161,4 @@ function incrementMoves(){
     }
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that 
- * you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 init();
